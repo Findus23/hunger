e33c68cb05a2526207332c1f6dceae0d585ba3b6
Lukas Winkler <l.winkler23@mailbox.org>
2017-06-08 11:20:52 +0200

    add SQL

---
+++ init.sql
@@ -0,0 +1,40 @@
+create table meals
+(
+	id int auto_increment
+		primary key,
+	name text not null,
+	description text null
+)
+;
+
+create table menus
+(
+	date date not null,
+	id int auto_increment
+		primary key,
+	venue int not null,
+	meal int not null,
+	constraint eindeutig
+		unique (venue, date),
+	constraint menus_meals_id_fk
+		foreign key (meal) references hunger.meals (id)
+)
+;
+
+create index menus_meals_id_fk
+	on menus (meal)
+;
+
+create table venues
+(
+	id int auto_increment
+		primary key,
+	name text null
+)
+;
+
+alter table menus
+	add constraint menus_ibfk_1
+		foreign key (venue) references hunger.venues (id)
+;
+
